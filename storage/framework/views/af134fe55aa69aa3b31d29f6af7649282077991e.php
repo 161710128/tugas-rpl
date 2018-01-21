337d46c8ab73b3c9898e22e8870f918d35e7b70d
161710151 <161710151@smkassalaambandung.sch.id>
2018-01-21 16:58:06 +0700

    first commit

---
+++ storage/framework/views/af134fe55aa69aa3b31d29f6af7649282077991e.php
@@ -0,0 +1,45 @@
+<?php $__env->startSection('konten'); ?>
+<table class="table">
+				<thead>
+					<tr>
+						<th>
+							#
+						</th>
+						<th>
+							Nama Siswa
+						</th>
+						<th>
+							Kelas
+						</th>
+						<th>
+							Jurusan
+						</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php  $no=1;  ?>
+					<?php $__currentLoopData = $a; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $tab): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+					<tr>
+						<td>
+							<?php echo e($no++); ?>
+
+						</td>
+						<td>
+							<?php echo e($tab->nama); ?>
+
+						</td>
+						<td>
+							<?php echo e($tab->kelas); ?>
+
+						</td>
+						<td>
+							<?php echo e($tab->jurusan); ?>
+
+						</td>
+					</tr>
+					<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+					
+				</body>
+			</table> 
+			<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layout.table', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
